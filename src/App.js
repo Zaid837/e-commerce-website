df034aaffc61945b4b77c405da1fdc98c8f26752
zaid837 <ayomide.zaid@gmail.com>
2021-02-24 14:50:08 +0100

    connected cart dropdown with cart reducer

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ const mapStateToProps = ({user}) => ({
   currentUser : user.currentUser
 })
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = dispatch => ({ 
   setCurrentUser : user => dispatch(setCurrentUser(user))
 });
 export default connect(mapStateToProps, mapDispatchToProps )(App);
--- src/redux/cart/cart-actions.js
+++ src/redux/cart/cart-actions.js
@@ -2,3 +2,8 @@ import {CardActionTypes} from "./cart-types";
 export const toggleClassHidden = () => ({
     type: CardActionTypes.TOGGLE_CART_DROPDOWN,
 });
+
+export const addItem = item => ({
+    type: CardActionTypes.ADD_ITEM,
+    payload: item
+})
