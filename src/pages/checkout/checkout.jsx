82313b07237016f1dd3417a0e32ec18d81ada0ee
zaid837 <ayomide.zaid@gmail.com>
2021-02-27 10:10:41 +0100

    added stripe payment testing to checkout

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -5,6 +5,7 @@ import {
   selectCartItems,
   selectCartTotal,
 } from "../../redux/cart/cart.selectors";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 import CheckoutItem from "../../components/checkout-item/checkout.component";
 
 import "./checkout.styles.scss";
@@ -35,6 +36,11 @@ const CheckoutPage = ({ cartItems, total }) => {
       <div className="total">
         <span>TOTAL: ${total}</span>
       </div>
+      <div className="test-warning">
+        *Please use the following test credit card for payments* <br /> 4242
+        4242 4242 4242 - Exp: 01/20 - CVV: 123
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
   );
 };
