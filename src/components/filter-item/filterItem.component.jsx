f5af5358a9a12ae9e4895ab917267cc7fe3f1804
zaid837 <ayomide.zaid@gmail.com>
2021-04-08 11:55:45 +0100

    commit

---
+++ src/components/filter-item/filterItem.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./filterItem.styles.scss";
+
+const FilterItems = () => {
+  return <div>Filter Items</div>;
+};
+
+export default FilterItems;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroBox from "../components/home/heroBox/herobox.component";
 import HomePage from "../components/home/homepage-component";
+import FilterItems from "../components/filter-item/filterItem.component";
 
 const Home = () => {
   return (
     <div>
       <HeroBox />
       <HomePage />
+      <FilterItems />
     </div>
   );
 };
